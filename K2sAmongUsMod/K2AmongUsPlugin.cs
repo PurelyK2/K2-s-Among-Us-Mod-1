ec4bb6e6a52f1e9b60f4876f52261b432469f8b8
PurelyK2 <k2purely@gmail.com>
2026-09-08 01:02:17 -0400

    Updated Version

--- K2sAmongUsMod/K2AmongUsPlugin.cs
+++ K2sAmongUsMod/K2AmongUsPlugin.cs
@@ -16,7 +16,7 @@ using UnityEngine;
 namespace K2AmongUs;
 
 /// <inheritdoc/>
-[BepInAutoPlugin("com.K2sAmongUs.mod", "K2sAmongUsMod", "0.1.6")]
+[BepInAutoPlugin("com.K2sAmongUs.mod", "K2sAmongUsMod", "0.1.7")]
 [BepInProcess("Among Us.exe")]
 [BepInDependency(ReactorPlugin.Id)]
 [BepInDependency(MiraApiPlugin.Id)]
